2b67417ef3aa3accf4314b07a46c173200ad7561
Ifeanyi <oisrael2000@gmail.com>
2023-02-20 20:50:38 +0100

    updated js file

--- tictac.js
+++ tictac.js
@@ -1,3 +1,4 @@
+
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
 const restartBtn = document.querySelector("#restartBtn");
@@ -11,9 +12,8 @@ const winConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-
-let options= ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X"
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
 let running = false;
 
 initializeGame();
@@ -24,7 +24,6 @@ function initializeGame(){
     statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
 }
-
 function cellClicked(){
     const cellIndex = this.getAttribute("cellIndex");
 
@@ -35,18 +34,14 @@ function cellClicked(){
     updateCell(this, cellIndex);
     checkWinner();
 }
-
 function updateCell(cell, index){
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
-    
 }
-
 function changePlayer(){
     currentPlayer = (currentPlayer == "X") ? "O" : "X";
     statusText.textContent = `${currentPlayer}'s turn`;
 }
-
 function checkWinner(){
     let roundWon = false;
 
@@ -59,25 +54,24 @@ function checkWinner(){
         if(cellA == "" || cellB == "" || cellC == ""){
             continue;
         }
-        if(cellA == cellB && cellC){
-            roundWon == true;
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
             break;
         }
+    }
 
-        if(roundWon){
-            statusText.textContent = `${currentPlayer} wins`
-            running = false;
-        }
-        else if(!options.includes("")){
-            statusText.textContent = `Draw!`;
-            running = FontFaceSetLoadEvent;
-        }
-        else{
-            changePlayer()
-        }
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
     }
 }
-
 function restartGame(){
     currentPlayer = "X";
     options = ["", "", "", "", "", "", "", "", ""];
